8e1ee1952869ffbf9b99d9f13a5c149d9b400f24
Meherab <jubayet0106@gmail.com>
2022-06-27 16:04:04 +0600

    set goals schema

--- Controllers/routeController.js
+++ Controllers/routeController.js
@@ -1,19 +1,26 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../model/goalModel");
 
 //method GET
 //url (api/goals)
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "All Goals" });
+  const goals = await Goal.find()
+  res.status(200).json(goals)
 });
 
 //method POST
 //url (api/goals)
 const postGoal = asyncHandler(async (req, res) => {
+  console.log(req.body.text);
   if (!req.body.text) {
     res.status(400);
     throw new Error("please fill text form");
   }
-  res.status(200).json({ message: "Post a Goal" });
+
+  const goal = await Goal.create({
+    text: req.body.text
+  });
+  res.status(200).json(goal);
 });
 
 //method PUT
