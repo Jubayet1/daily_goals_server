97216802449ec7dd941234661795e6b80bdc7cfa
Meherab <jubayet0106@gmail.com>
2022-06-27 15:33:00 +0600

    data base connected

--- Controllers/routeController.js
+++ Controllers/routeController.js
@@ -1,30 +1,32 @@
+const asyncHandler = require("express-async-handler");
+
 //method GET
 //url (api/goals)
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "All Goals" });
-};
+});
 
 //method POST
 //url (api/goals)
-const postGoal = (req, res) => {
-  if(!req.body.text){
-    res.status(400)
-    throw new Error('please fill text form')
+const postGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("please fill text form");
   }
   res.status(200).json({ message: "Post a Goal" });
-};
+});
 
 //method PUT
 //url (api/goals/:id)
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update ${req.params.id}` });
-};
+});
 
 //method DELETE
 //url (api/goals/:id)
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
